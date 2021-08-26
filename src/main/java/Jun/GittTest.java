f2e61ac8b40bacd7daa3e35f01a7447f7c09f4f2
Jun <Jun@ECUT.com>
2021-08-26 19:54:01 +0800

    fitst commit

---
+++ src/main/java/Jun/GittTest.java
@@ -0,0 +1,11 @@
+package Jun;
+
+/**
+ * @author rookie
+ * @create 2021-08-26-19:48
+ */
+public class GittTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
